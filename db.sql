d71424f841703df6f72f44af756190be65cfe503
Jason Liang <60914103+xurong-liang@users.noreply.github.com>
2022-03-31 21:40:34 +1000

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,15 +1,11 @@
 
 CREATE TABLE IF NOT EXISTS `phone_book` (
-  `id` int(5) NOT NULL,
+  `id` int(5) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `phone` varchar(50) NOT NULL,
   `address` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+);
 
 
-INSERT INTO `phone_book` (`id`, `name`, `phone`, `address`) VALUES
-(1, 'Admin', '0432453323', 'St Lucia');
-
-
-ALTER TABLE `phone_book`
-  ADD PRIMARY KEY (`id`);
+INSERT INTO `phone_book` (`name`, `phone`, `address`) VALUES
+('Admin', '0432453323', 'St Lucia');
